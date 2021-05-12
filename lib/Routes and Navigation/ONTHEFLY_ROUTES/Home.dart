6124344b15ba1b5ca36d70e6ac95411bbfff5191
bishwayanbanerjee <bishwayanofficial@gmail.com>
2021-05-12 20:24:25 +0530

    just a delete

--- lib/Routes and Navigation/ONTHEFLY_ROUTES/Home.dart
+++ lib/Routes and Navigation/ONTHEFLY_ROUTES/Home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:practising_app/Routes%20and%20Navigation/ONTHEFLY_ROUTES/2nd_page.dart';
+
 
 class Home1 extends StatelessWidget {
   @override
